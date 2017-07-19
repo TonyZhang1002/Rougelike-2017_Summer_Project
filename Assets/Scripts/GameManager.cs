f917279b8b0ba24cacd7f1fd226a4c15e0d9c213
Tony_NUYida <TonyZhang1002@users.noreply.github.com>
2017-07-19 10:18:04 +0800

    Player is now able to move and enemies will attack

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-	
+
+	public float turnDelay = .1f;
 	public static GameManager instance = null;
 	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool players = true;
 
 	private int level = 3;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
 
 	// Use this for initialization
 	void Awake () {
@@ -18,16 +23,67 @@ public class GameManager : MonoBehaviour {
 		}
 
 		DontDestroyOnLoad (gameObject);
+		enemies = new List<Enemy>();
 		boardScript = GetComponent<BoardManager> ();
 		InitGame ();
 	}
 
 	void InitGame(){
+		enemies.Clear ();
 		boardScript.SetupScene (level);
+	}
+
+	public void gameOver(){
+		enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//Check that playersTurn or enemiesMoving or doingSetup are not currently true.
+		if(players || enemiesMoving)
+
+			//If any of these are true, return and do not start MoveEnemies.
+			return;
+
+		//Start moving enemies.
+		StartCoroutine (MoveEnemies ());
+	}
+
+	public void AddEnemyToList(Enemy script)
+	{
+		//Add Enemy to List enemies.
+		enemies.Add(script);
+	}
+
+	//Coroutine to move enemies in sequence.
+	IEnumerator MoveEnemies()
+	{
+		//While enemiesMoving is true player is unable to move.
+		enemiesMoving = true;
+
+		//Wait for turnDelay seconds, defaults to .1 (100 ms).
+		yield return new WaitForSeconds(turnDelay);
+
+		//If there are no enemies spawned (IE in first level):
+		if (enemies.Count == 0) 
+		{
+			//Wait for turnDelay seconds between moves, replaces delay caused by enemies moving when there are none.
+			yield return new WaitForSeconds(turnDelay);
+		}
+
+		//Loop through List of Enemy objects.
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			//Call the MoveEnemy function of Enemy at index i in the enemies List.
+			enemies[i].MoveEnemy ();
+
+			//Wait for Enemy's moveTime before moving next Enemy, 
+			yield return new WaitForSeconds(enemies[i].moveTime);
+		}
+		//Once Enemies are done moving, set playersTurn to true so player can move.
+		players = true;
+
+		//Enemies are done moving, set enemiesMoving to false.
+		enemiesMoving = false;
 	}
 }
