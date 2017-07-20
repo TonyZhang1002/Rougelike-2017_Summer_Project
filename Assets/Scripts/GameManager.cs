b8f3f70b879fb9afcc770b204839467b4f8db19e
Tony_NUYida <TonyZhang1002@users.noreply.github.com>
2017-07-20 17:55:05 +0800

    All done here

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+	public float levelStartDelay = 2f;
 	public float turnDelay = .1f;
 	public static GameManager instance = null;
 	public BoardManager boardScript;
 	public int playerFoodPoints = 100;
 	[HideInInspector] public bool players = true;
 
-	private int level = 3;
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
 	private List<Enemy> enemies;
 	private bool enemiesMoving;
+	private bool doingSetup;
 
 	// Use this for initialization
 	void Awake () {
@@ -28,19 +33,50 @@ public class GameManager : MonoBehaviour {
 		InitGame ();
 	}
 
+	//This is called each time a scene is loaded.
+	void OnLevelWasLoaded(int index)
+	{
+		//Add one to our level number.
+		level++;
+		//Call InitGame to initialize our level.
+		InitGame();
+	}
+
 	void InitGame(){
+
+		doingSetup = true;
+
+		levelImage = GameObject.Find ("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text> ();
+		levelText.text = "Day " + level;
+		levelImage.SetActive (true);
+		Invoke ("HideLevelImage", levelStartDelay);
+
 		enemies.Clear ();
 		boardScript.SetupScene (level);
 	}
 
+	//Hides black image used between levels
+	private void HideLevelImage()
+	{
+		//Disable the levelImage gameObject.
+		levelImage.SetActive(false);
+
+		//Set doingSetup to false allowing player to move again.
+		doingSetup = false;
+	}
+
+
 	public void gameOver(){
+		levelText.text = "After "+level+" days, you starved.";
+		levelImage.SetActive (true);
 		enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Check that playersTurn or enemiesMoving or doingSetup are not currently true.
-		if(players || enemiesMoving)
+		if(players || enemiesMoving || doingSetup)
 
 			//If any of these are true, return and do not start MoveEnemies.
 			return;
